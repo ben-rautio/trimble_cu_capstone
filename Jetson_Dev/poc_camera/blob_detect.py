6ff961515515200a3f08bbeb64de9b5b4aec7151
trimble <bera3945@colorado.edu>
2021-11-19 13:46:53 -0700

    using erosion for poc to get rid of noise

--- Jetson_Dev/poc_camera/blob_detect.py
+++ Jetson_Dev/poc_camera/blob_detect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def gstreamer_pipeline(
     capture_width=3264,
@@ -31,17 +32,33 @@ def gstreamer_pipeline(
 def show_camera():
 # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=2))
+    #MORPH_CROSS
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
+    #regular ones kernel
+    kernel_ones = np.ones((5,5), np.uint8)
     cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=2), cv2.CAP_GSTREAMER)
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
         while cv2.getWindowProperty("CSI Camera", 0) >= 0:
             ret_val, img = cap.read()
+            #blur = cv2.GaussianBlur(img,(5,5),0)
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #Note here: when put behind k850 filter, there is much more noise picked up, why??
             ret, thresh = cv2.threshold(img_gray, 250, 255, cv2.THRESH_BINARY)
+            thresh = cv2.erode(thresh, np.ones((7,7), np.uint8), iterations=2)
+            #thresh = cv2.erode(thresh, kernel, iterations=2)
+            img = cv2.GaussianBlur(img,(5,5),0)
+            img = cv2.erode(img, kernel, iterations=3)
+            #otsu's thresholding
+            #ret, thresh = cv2.threshold(img_gray, 253, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+            #blur = cv2.GaussianBlur(thresh,(5,5),0)
             contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+            print(len(contours))
+
             for c in contours:
-                # calculate moments for each contour
+                #print(c)
+                #calculate moments for each contour
                 M = cv2.moments(c)
                 if M["m00"] != 0:
                     cX = int(M["m10"] / M["m00"])
@@ -50,12 +67,15 @@ def show_camera():
                 else:
                     cX, cY = 0, 0
 
-
-                #image_copy = img.copy()
-                # cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=2, lineType=cv2.LINE_AA)
+            #having trouble printing out the contours for some reason
+            #image_copy = blur.copy()
+            cv2.drawContours(image=img, contours=contours, contourIdx=-1, color=(255, 0, 0), thickness=5, lineType=cv2.LINE_AA)
+    
                 # cv2.circle(image_copy, (cX, cY), 5, (255, 255, 255), -1)
-                cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-            cv2.imshow('CSI Camera', img)
+                #cv2.putText(img, "centroid", (cX - 25, cY - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+            #cv2.imshow('contours', img)
+           #cv2.imshow("blur", blur)
+            cv2.imshow("binary image", thresh)
             #print("before imshow")
             #cv2.waitKey(3000)
             #print("after imshow")
@@ -63,12 +83,12 @@ def show_camera():
 
         #cv2.destroyAllWindows()
 
-            #cv2.imshow("CSI Camera", img)
+            cv2.imshow("CSI Camera", img)
             #This also acts as
             keyCode = cv2.waitKey(30) & 0xFF
             #Stop the program on the ESC key
             if keyCode == 27:
-                cv2.imwrite('image_thres1.jpg', img)
+                #cv2.imwrite('image_thres1.jpg', blur)
                 break
         cap.release()
         cv2.destroyAllWindows()
