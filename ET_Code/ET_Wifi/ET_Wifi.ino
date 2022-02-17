842e9d5d2522fd9ae3350f735d03d52f2298141f
Alexander Bork <albo7471@colorado.edu>
2022-02-17 09:20:10 -0700

    Final ET_Wifi code with UART

--- ET_Code/ET_Wifi/ET_Wifi.ino
+++ ET_Code/ET_Wifi/ET_Wifi.ino
@@ -1,4 +1,8 @@
 #include <AsyncTCP.h>
+#include <SerialTransfer.h>
+#include "WiFi.h"
+#include "HTTPClient.h"
+#include "ESPAsyncWebServer.h"
 /*
   Rui Santos
   Complete project details at https://RandomNerdTutorials.com/esp32-client-server-wi-fi/
@@ -10,20 +14,12 @@
   copies or substantial portions of the Software.
 */
 
-// 
-
-
-// Import required libraries
-#include "WiFi.h"
-#include "HTTPClient.h"
-#include "ESPAsyncWebServer.h"
-
 // Set your access point network credentials
 const char* ssid = "ESP32-Access-Point";
 const char* password = "123456789";
 
 // Different servers
-const char* serverStatus = "http://192.168.4.1/status";
+//const char* serverStatus = "http://192.168.4.1/status";
 const char* serverChange0 = "http://192.168.4.1/change?dc=0";
 const char* serverChange10 = "http://192.168.4.1/change?dc=10";
 const char* serverChange20 = "http://192.168.4.1/change?dc=20";
@@ -35,59 +31,102 @@ const char* serverChange70 = "http://192.168.4.1/change?dc=70";
 const char* serverChange80 = "http://192.168.4.1/change?dc=80";
 const char* serverChange90 = "http://192.168.4.1/change?dc=90";
 const char* serverChange100 = "http://192.168.4.1/change?dc=100";
-const char* serverChange110 = "http://192.168.4.1/change?dc=110";
+//const char* serverChange110 = "http://192.168.4.1/change?dc=110";
 const char* serverBork = "http://192.168.4.1/bork";
 const char* serverBen = "http://192.169.4.1/";
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+SerialTransfer myTransfer;
+bool updated;
+struct STRUCT {
+  uint32_t z;
+} typedef testStruct;
 
+testStruct myTestStruct;
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
+  Serial1.begin(115200);
   Serial.println();
   Serial.println("Connecting...");
   WiFi.begin(ssid);
+  updated = true;
   while(WiFi.status() != WL_CONNECTED){
-    delay(500);
+    delay(1000);
     Serial.print("REEEE");
   }
   Serial.print("Connected to WiFi network w/ IP: ");
+  Serial.println("localIP access");
   Serial.println(WiFi.localIP());
+  myTransfer.begin(Serial1);
+  myTestStruct.z = 0;
 }
 
-String Bork;
-String dutyCycle;
-
 void loop(){
-//   delay(5000);
-//   dutyCycle = getDuty(serverChange);
-//   Bork = getDuty(serverBork);
-//   Serial.println(dutyCycle);
-//   Serial.println(Bork);
-  dutyCycle = getDuty(serverChange0).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange10).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange20).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange30).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange40).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange50).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange60).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange70).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange80).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange90).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange100).toInt();
-  delay(3000);
-  dutyCycle = getDuty(serverChange110).toInt();
-  delay(3000);
+    if(myTransfer.available())
+    {
+      Serial.println("Available");
+      // use this variable to keep track of how many
+      // bytes we've processed from the receive buffer
+      uint16_t recSize = 0;
+      recSize = myTransfer.rxObj(myTestStruct.z, recSize);
+      Serial.print(myTestStruct.z);
+      Serial.print(" | ");
+      updated = true;
+    }
+    //Serial.println("switch statements");
+    if(updated){
+      switch(myTestStruct.z){
+        case 0:
+          //Serial.print("Case 0");
+          getDuty(serverChange0);
+          updated = false;
+          break;
+        case 10:
+          getDuty(serverChange10);
+          updated = false;
+          break;
+        case 20:
+          getDuty(serverChange20);
+          updated = false;
+          break;
+        case 30:
+          getDuty(serverChange30);
+          updated = false;
+          break;
+        case 40:
+          getDuty(serverChange40);
+          updated = false;
+          break;
+        case 50:
+          getDuty(serverChange50);
+          updated = false;
+          break;
+        case 60:
+          getDuty(serverChange60);
+          updated = false;
+          break;
+        case 70:
+          getDuty(serverChange70);
+          updated = false;
+          break;
+        case 80:
+          getDuty(serverChange80);
+          updated = false;
+          break;
+        case 90:
+          getDuty(serverChange90);
+          updated = false;
+          break;
+        case 100:
+          getDuty(serverChange100);
+          updated = false;
+          break;
+        default:
+          updated = false;
+          break;
+      }  
+    }
 }
 
 String getDuty(const char* serverName){
@@ -96,8 +135,8 @@ String getDuty(const char* serverName){
   int httpResp = http.GET();
   String payload = "Not right";
   if(httpResp > 0){
-    Serial.print("HTTP Response: ");
-    Serial.println(httpResp);
+    //Serial.print("HTTP Response: ");
+    //Serial.println(httpResp);
     payload = http.getString();
   }
   else{
@@ -107,14 +146,3 @@ String getDuty(const char* serverName){
   http.end();
   return payload;
 }
-
-//String getStatus(
-//void getBen(){
-//  WiFiClient client;
-//  HTTPClient http;
-//  http.begin(client, serverBen);
-//  Serial.print("Ben Request: ");
-////  Serial.println(http.getString());
-//  http.end();
-//  return;
-//}
