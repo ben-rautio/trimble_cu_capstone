c6a09e239e7c472f85e9dd8d7d49d90f937a1687
Ben Rautio <ben.e.rautio@gmail.com>
2022-03-18 13:12:08 -0600

    ET code changes

--- ET_Code/ET_UART_WiFi/ET_UART_WiFi.ino
+++ ET_Code/ET_UART_WiFi/ET_UART_WiFi.ino
@@ -14,6 +14,17 @@
   copies or substantial portions of the Software.
 */
 
+#define RXD2 9
+#define TXD2 10
+#define TX_WIFI 34
+#define REQ_UART 33
+#define HEARTBEAT 32
+
+#define WIFI_CONNECTED 25
+#define HB_LED 26
+#define LED4 27
+
+
 // Set your access point network credentials
 const char* ssid = "ESP32-Access-Point";
 const char* password = "123456789";
@@ -34,37 +45,105 @@ const char* serverChange100 = "http://192.168.4.1/change?dc=100";
 //const char* serverChange110 = "http://192.168.4.1/change?dc=110";
 const char* serverBork = "http://192.168.4.1/bork";
 const char* serverBen = "http://192.169.4.1/";
+
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+
 SerialTransfer myTransfer;
 bool updated;
+
 struct STRUCT {
   uint32_t z;
 } typedef testStruct;
 
 testStruct myTestStruct;
+
+void blink(int pin)
+{
+  delay(100);
+  digitalWrite(pin, HIGH);
+  delay(100);
+  digitalWrite(pin, LOW);
+}
+
+unsigned long currentMillis = millis();
+unsigned long previousMillis = 0;
+int ledState = LOW;             
+const long interval = 1000;          
+
+void HB()
+{
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+  }
+}
+unsigned long wifiCurrent = millis();
+unsigned long wifiPrevious = 0;
+int wifiStateLED = LOW;             
+const long wifiInterval = 5000;
+
+void wifiStatus()
+{
+  if (wifiCurrent - wifiPrevious >= wifiInterval) {
+    // save the last time you blinked the LED
+    wifiStateLED = LOW;
+  }
+  else {
+    wifiStateLED = HIGH;
+  }
+}
+
 void setup(){
-  // Serial port for debugging purposes
+  pinMode(TX_WIFI, OUTPUT);
+  pinMode(HEARTBEAT, OUTPUT);
+  pinMode(REQ_UART, OUTPUT);
+  pinMode(WIFI_CONNECTED, OUTPUT);
+  pinMode(HB_LED, OUTPUT);
+  
+  digitalWrite(WIFI_CONNECTED, LOW);
+  
   Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial2.begin(115200);
   Serial.println();
   Serial.println("Connecting...");
   WiFi.begin(ssid);
   updated = true;
   while(WiFi.status() != WL_CONNECTED){
     delay(1000);
-    Serial.print("REEEE");
+     Serial.print("REEEE");
   }
+  digitalWrite(WIFI_CONNECTED, HIGH);
   Serial.print("Connected to WiFi network w/ IP: ");
   Serial.println("localIP access");
   Serial.println(WiFi.localIP());
-  myTransfer.begin(Serial1);
+  myTransfer.begin(Serial2);
   myTestStruct.z = 0;
 }
 
 void loop(){
+
+    //Heartbeat LED
+    currentMillis = millis();
+    wifiCurrent = millis();
+    
+    HB();
+    digitalWrite(HB_LED, ledState);
+
+    wifiStatus();
+
+    //check uart
     if(myTransfer.available())
     {
+      //blink(REQ_UART);
+      digitalWrite(REQ_UART, HIGH);
       Serial.println("Available");
       // use this variable to keep track of how many
       // bytes we've processed from the receive buffer
@@ -81,52 +160,75 @@ void loop(){
           //Serial.print("Case 0");
           getDuty(serverChange0);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 10:
           getDuty(serverChange10);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 20:
           getDuty(serverChange20);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 30:
           getDuty(serverChange30);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 40:
           getDuty(serverChange40);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 50:
           getDuty(serverChange50);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 60:
           getDuty(serverChange60);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 70:
           getDuty(serverChange70);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 80:
           getDuty(serverChange80);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 90:
           getDuty(serverChange90);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         case 100:
           getDuty(serverChange100);
           updated = false;
+          digitalWrite(REQ_UART, LOW);
+          wifiPrevious = wifiCurrent;
           break;
         default:
           updated = false;
           break;
       }  
     }
+    blink(HEARTBEAT);
 }
 
 String getDuty(const char* serverName){
